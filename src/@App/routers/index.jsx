a66fff30e11d0589470b31edd2996524a7dcea73
anhnt8102003 <anhnt08102003@gmail.com>
2023-10-23 18:00:01 +0700

    refactor: update index.js

--- src/@App/routers/index.jsx
+++ src/@App/routers/index.jsx
@@ -8,20 +8,60 @@ import CommonLayout from '@App/components/layouts/common';
 
 import PublicRouter from './components/PublicRouter';
 import PrivateRouter from './components/PrivateRouter';
-import Home from '@App/pages/Admin/Home';
+import { routerPath } from '@App/configs/routerConfig';
 
-// const Signin = Loadable(lazy(() => import('@App/pages/Signin')));
+import brandRouter from './admin/brand.router';
+import productRouter from './admin/product.router';
+import Home from '@App/pages/Views/Home';
+import Cart from '@App/pages/Views/Cart';
+import Shipping from '@App/pages/Views/Shipping';
+import categoryRouter from './admin/category.router';
+
+const Signin = Loadable(lazy(() => import('@App/pages/Auth/Signin')));
+const Register = Loadable(lazy(() => import('@App/pages/Auth/Register')));
+const Product = Loadable(lazy(() => import('@App/pages/Views/Product')));
+const ProductDetail = Loadable(lazy(() => import('@App/pages/Views/ProductDetail')));
 // const GetCurrentUser = lazy(() => import('@App/pages/Signin/GetCurrentUser'));
 
 const routers = [
    {
-      path: 'signin',
-      element: <PublicRouter />,
+      path: '/',
+      element: <CommonLayout />,
       children: [
-         // {
-         //    index: true,
-         //    element: <Signin />
-         // },
+         {
+            index: true,
+            element: <Home />
+         },
+         {
+            path: routerPath.PRODUCTS,
+            element: <Product />
+         },
+         {
+            path: routerPath.PRODUCTS + '/:id',
+            element: <ProductDetail />
+         },
+         {
+            path: routerPath.CART,
+            element: <Cart />
+         },
+         {
+            path: routerPath.SHIPPING,
+            element: <Shipping />
+         },
+         {
+            path: '/',
+            element: <PublicRouter />,
+            children: [
+               {
+                  path: 'signin',
+                  element: <Signin />
+               },
+               {
+                  path: 'register',
+                  element: <Register />
+               }
+            ]
+         }
          // {
          //    path: 'success',
          //    element: <GetCurrentUser />
@@ -29,32 +69,25 @@ const routers = [
       ]
    },
    {
-      path: '/',
-      element: (
-         // <PrivateRouter>
-         <CommonLayout />
-         // </PrivateRouter>
-      ),
-      children: [
-         {
-            index: true,
-            element: <Home />
-         }
-      ]
-   },
-   {
       path: '/admin',
       element: (
-         // <PrivateRouter>
-         <AdminLayout />
-         // </PrivateRouter>
+         <PrivateRouter>
+            <AdminLayout />
+         </PrivateRouter>
       ),
       children: [
          {
             index: true,
             element: <Outlet />
          },
+         categoryRouter,
+         brandRouter,
+         productRouter
       ]
+   },
+   {
+      path: '*',
+      element: <h1>Đường đãn không tồn tại</h1>
    }
 ];
 
