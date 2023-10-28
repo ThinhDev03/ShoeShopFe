60eef797c8aea562edf67e6af4096d0f10345751
PH27311 <tundph27311@fpt.edu.vn>
2023-10-28 11:54:54 +0700

    ProductDetail

--- src/@App/pages/Admin/Product/ProductDetail.jsx
+++ src/@App/pages/Admin/Product/ProductDetail.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
-import BasicPage from '@App/components/customs/BasicPage';
-import BaseFormProductDetail from './components/BaseFormProductDetail';
-import { yupResolver } from '@hookform/resolvers/yup';
-import yupProductDetail from './utils/yupProductDetail';
 import { useParams } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
-import productDetail from '@App/services/product-detail.service';
-import { routerPath } from '@App/configs/routerConfig';
+import { Box, Typography } from '@mui/material';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-function ProductDetail() {
-   const { id } = useParams();
+import { routerPath } from '@App/configs/routerConfig';
+import yupProductDetail from './utils/yupProductDetail';
+import BasicPage from '@App/components/customs/BasicPage';
+import productService from '@App/services/product.service';
+import productDetail from '@App/services/product-detail.service';
+import { errorMessage, successMessage } from '@Core/Helper/Message';
+import BaseFormProductDetail from './components/BaseFormProductDetail';
+import { useMutation, useQueries, useQuery } from '@tanstack/react-query';
+import yupDetail from './utils/yupProductDetail';
+
+function ProductDetail({ id }) {
    const form = useForm({
       mode: 'onChange',
-      resolver: yupResolver(yupProductDetail),
-      defaultValues: yupProductDetail.getDefault()
+      resolver: yupResolver(yupDetail),
+      defaultValues: {
+         details: [
+            {
+               size_id: '',
+               color_id: '',
+               quantity: '',
+               price: '',
+               sale: '',
+               image_id: ''
+            }
+         ]
+      }
+   });
+
+   const { data } = useQuery(['getProductItem', id], async () => {
+      try {
+         await productService.getOne(id);
+         return true;
+      } catch (error) {
+         errorMessage('Sản phẩm không tồn tại');
+      }
    });
 
    const { isLoading, mutate } = useMutation({
@@ -33,8 +56,9 @@ function ProductDetail() {
    });
 
    const onSubmit = (data) => {
-      const dataProduct = { ...data, product_id: id };
-      mutate(dataProduct);
+      const { variation } = data;
+      const newData = variation.map((item) => ({ ...item, product_id: id }));
+      mutate(newData);
    };
 
    const breadcrumbs = [
@@ -48,11 +72,27 @@ function ProductDetail() {
       }
    ];
 
-   return (
-      <BasicPage currentPage='Thêm detail sản phẩm' breadcrumbs={breadcrumbs}>
-         <BaseFormProductDetail form={form} onSubmit={onSubmit} loading={isLoading} />
-      </BasicPage>
+   return data ? (
+      <BaseFormProductDetail form={form} onSubmit={onSubmit} loading={isLoading} />
+   ) : (
+      <Box
+         sx={{
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column'
+         }}>
+         <Typography variant='h5' sx={{ color: '#ABAEB0' }}>
+            Sản phẩm chính không tồn tại.
+         </Typography>
+         <Box component='p' sx={{ color: '#ABAEB0' }}>
+            vui lòng thêm sản phẩm chính trước khi thêm các biến thể
+         </Box>
+      </Box>
    );
 }
+
+
 
 export default ProductDetail;
