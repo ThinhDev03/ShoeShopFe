69375b2f4e587a424cb3cded7a81e9e87f6a074c
Bacnxph19667 <nxb167200@gmail.com>
2023-11-03 13:14:38 +0700

    update color

--- src/@App/pages/Admin/Color/index.jsx
+++ src/@App/pages/Admin/Color/index.jsx
@@ -27,7 +27,7 @@ export default function Color() {
          return await colorService.deleteColor(data.id);
       },
       onSuccess: () => {
-         successMessage('Xóa sản phảm thành công');
+         successMessage('Xóa sản phẩm thành công');
          getCategory();
       }
    });
@@ -75,7 +75,7 @@ export default function Color() {
                               id: subject?._id
                            })
                         }
-                        content='Bạn có muốn xoá môn học này?'
+                        content='Bạn có chắc chắn muốn xoá?'
                      />
                   </Box>
                );
