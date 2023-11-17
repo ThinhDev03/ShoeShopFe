695ab3efb36a13cce6ac0ffb426e4a29af946dcb
CuongBui157 <cuongbui1572003@gmail.com>
2023-11-17 22:15:17 +0700

    feat: UpdateBill

--- src/@App/pages/Admin/Bill/UpdateBill.jsx
+++ src/@App/pages/Admin/Bill/UpdateBill.jsx
@@ -7,68 +7,78 @@ import BaseFormBill from './components/BaseFormBill';
 import billService from '@App/services/bill.service';
 import { billStatus } from './utils';
 import { errorMessage, successMessage } from '@Core/Helper/Message';
+import { useConfirm } from '@Core/Components/Confirm/CoreConfirm';
 
 export default function UpdateBill() {
-   const { id } = useParams();
-   const navigate = useNavigate();
-   const form = useForm({
-      mode: 'onChange'
-   });
-   const { data } = useQuery(
-      ['getListBill', id],
-      async () => {
-         const rest = await billService.find('by-id/' + id);
-         return rest;
-      },
-      {
-         onSuccess(data) {
-            const extendData = data.data;
-            console.log(extendData.payment_id.payment_method);
-            form.reset({
-               ...extendData,
-               user_order: extendData.user_id?.fullname,
-               payment_method: extendData?.payment_id ? extendData.payment_id.payment_method : '',
-               payment_status: extendData?.payment_id ? extendData.payment_id.status : ''
-            });
-         },
-         initialData: { billDetail: [] }
-      }
-   );
-   const { mutate: onChangeStatus } = useMutation({
-      mutationFn: async ({ id, status }) => {
-         return await billService.updateStatus(id, status);
-      },
-      onSuccess: () => {
-         successMessage('Cập nhật đơn hàng thành công');
-         navigate('/admin/bill');
-      },
-      onError: (err) => {
-         errorMessage('Cập nhật đơn hàng thất bại');
-      }
-   });
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const confirm = useConfirm();
 
-   const onSubmit = (data) => {
-      onChangeStatus({ id: data._id, status: data.status });
-   };
-   const props = {
-      billDetail: data.billDetail,
-      billStatus,
-      form,
-      onSubmit
-   };
-   const breadcrumbs = [
-      {
-         name: 'Trang chủ',
-         link: '/'
-      },
-      {
-         name: 'Đơn hàng',
-         link: '/bill'
-      }
-   ];
-   return (
-      <BasicPage currentPage='Cập nhật' breadcrumbs={breadcrumbs}>
-         <BaseFormBill {...props} />
-      </BasicPage>
-   );
+    const form = useForm({
+        mode: 'onChange'
+    });
+    const { data } = useQuery(
+        ['getListBill', id],
+        async () => {
+            const rest = await billService.find('by-id/' + id);
+            return rest;
+        },
+        {
+            onSuccess(data) {
+                const extendData = data.data;
+                console.log(extendData.payment_id.payment_method);
+                form.reset({
+                    ...extendData,
+                    user_order: extendData.user_id?.fullname,
+                    payment_method: extendData?.payment_id ? extendData.payment_id.payment_method : '',
+                    payment_status: extendData?.payment_id ? extendData.payment_id.status : ''
+                });
+            },
+            initialData: { billDetail: [] }
+        }
+    );
+    const { mutate: onChangeStatus } = useMutation({
+        mutationFn: async ({ id, status }) => {
+            return await billService.updateStatus(id, status);
+        },
+        onSuccess: () => {
+            successMessage('Cập nhật đơn hàng thành công');
+            navigate('/admin/bill');
+        },
+        onError: (err) => {
+            errorMessage('Cập nhật đơn hàng thất bại');
+        }
+    });
+
+    const onSubmit = (data) => {
+        confirm({
+            title: 'Cập nhật trạng thái đơn hàng.',
+            content: 'Bạn có chắc muốn cập nhật trạng thái của đơn hàng?',
+            okText: 'Cập nhật',
+            onOk: () => {
+                onChangeStatus({ id: data._id, status: data.status });
+            }
+        });
+    };
+    const props = {
+        billDetail: data.billDetail,
+        billStatus,
+        form,
+        onSubmit
+    };
+    const breadcrumbs = [
+        {
+            name: 'Trang chủ',
+            link: '/'
+        },
+        {
+            name: 'Đơn hàng',
+            link: '/bill'
+        }
+    ];
+    return (
+        <BasicPage currentPage='Cập nhật' breadcrumbs={breadcrumbs}>
+            <BaseFormBill {...props} />
+        </BasicPage>
+    );
 }
