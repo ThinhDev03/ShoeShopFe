382ef748304872bdaf33fe2fca8c38461c4da8bd
Nguyen Tuan Anh <anhnt08102003@gmail.com>
2023-11-30 16:49:28 +0700

    update validate product

--- src/@App/pages/Admin/Product/utils/yupProduct.js
+++ src/@App/pages/Admin/Product/utils/yupProduct.js
@@ -2,15 +2,25 @@ import * as yup from 'yup';
 import Regex from '@Core/Helper/Regex';
 
 const yupProduct = yup.object().shape({
-   name: yup.string().trim('Vui lòng bỏ khoảng trống').strict(true).required('Vui lòng nhập tên màu').default(''),
-   category_id: yup.string().trim('Vui lòng bỏ khoảng trống').strict(true).required('Không được để trống').default(''),
-   brand_id: yup.string().trim('Vui lòng bỏ khoảng trống').strict(true).required('Vui lòng nhập mã màu').default(''),
+   name: yup.string().trim('Vui lòng bỏ khoảng trống').strict(true).required('Vui lòng nhập tên sản phẩm').default(''),
+   category_id: yup
+      .string()
+      .trim('Vui lòng bỏ khoảng trống')
+      .strict(true)
+      .required('Vui lòng chọn danh mục')
+      .default(''),
+   brand_id: yup
+      .string()
+      .trim('Vui lòng bỏ khoảng trống')
+      .strict(true)
+      .required('Vui lòng chọn thương hiệu')
+      .default(''),
    description: yup
       .string()
       .required()
       .trim('Vui lòng bỏ khoảng trống')
       .strict(true)
-      .required('Không được để trống')
+      .required('Vui lòng nhập mô tả')
       .default(''),
    thumbnail: yup.string().trim('Vui lòng bỏ khoảng trống').required('Không được để trống').default(''),
    images: yup.array().required('Không được để trống').default([]),
