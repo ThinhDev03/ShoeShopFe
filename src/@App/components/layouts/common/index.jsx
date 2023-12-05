51760be3036db090dca9a1a523a8efeef1bb5ef9
Thinhnvph26188 <nguyenthinhgt2@gmail.com>
2023-12-05 20:14:53 +0700

    update

--- src/@App/components/layouts/common/index.jsx
+++ src/@App/components/layouts/common/index.jsx
@@ -5,7 +5,6 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Box } from '@mui/material';
-
 export default function CommonLayout() {
    return (
       <>
