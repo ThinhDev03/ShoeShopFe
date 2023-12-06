2f636e007e7d62bcbb9698127d8b4e7aa5ef2b46
tuannmph27409 <tuannmph27409@fpt.edu.vn>
2023-12-06 21:54:43 +0700

    fix voucher

--- src/@App/pages/Admin/Voucher/components/BaseFormVoucher.jsx
+++ src/@App/pages/Admin/Voucher/components/BaseFormVoucher.jsx
@@ -5,10 +5,10 @@ import React from 'react';
 import SaveIcon from '@mui/icons-material/Save';
 import FormLabel from '@Core/Components/FormControl/FormLabel';
 
+
 function BaseFormVoucher(props) {
    const { form, onSubmit } = props;
    const { control, handleSubmit } = form;
-
    return (
       <Box component='form' onSubmit={handleSubmit(onSubmit)}>
          <Grid container spacing={3}>
--- src/@App/pages/Admin/Voucher/utils/yupVoucher.js
+++ src/@App/pages/Admin/Voucher/utils/yupVoucher.js
@@ -10,23 +10,23 @@ yup.setLocale({
 const yupVoucher = yup.object().shape({
    voucher_name: yup
       .string()
-      .trim('Vui lòng bỏ khoảng trống')
+      .trim('Vui lòng bỏ khoảng trống ')
       .strict(true)
-      .required('Vui lòng nhập tên voucher')
+      .required('Vui lòng nhập tên voucher ')
       .default(''),
    point_discount: yup
       .string()
       .trim('Vui lòng bỏ khoảng trống')
       .strict(true)
-      .required('Vui lòng nhập điều kiện giảm giá')
-      .matches(Regex.number, 'Điều kiện giảm giá phải là số')
+      .required('Vui lòng nhập điều kiện giảm giá ')
+      .matches(Regex.number, 'Điều kiện giảm giá phải là số ')
       .default(''),
    discount: yup
       .string()
       .trim('Vui lòng bỏ khoảng trống')
       .strict(true)
       .required('Vui lòng nhập số tiền giảm')
-      .matches(Regex.number, 'Số tiền giảm phải là số')
+      .matches(Regex.number, 'Số tiền giảm phải là số ')
       .default('')
 });
 export default yupVoucher;
